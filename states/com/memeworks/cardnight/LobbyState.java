d549214a368281c28234abebea44c2505b86ff51
kolibabas <stephen.kolibaba@gmail.com>
2011-08-31 18:21:28 -0700

    Added LobbyState

---
+++ states/com/memeworks/cardnight/LobbyState.java
@@ -0,0 +1,34 @@
+package com.memeworks.cardnight;
+
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+
+public class LobbyState implements GameState {
+
+
+	public void Frame_Started(float frame_time) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean Touch_Event(MotionEvent evt) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void Enter() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void Leave() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Canvas Draw(Canvas c) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
